6e30910386edb3ef7149a1cf35aadf4f639135ac
Fifolu Akinyemi <fkinyemi@gmail.com>
2024-01-19 19:42:00 +0000

    WIP Crawler for AirBnB

--- src/main/java/com/example/holidayplanner/crawler/Crawler.java
+++ src/main/java/com/example/holidayplanner/crawler/Crawler.java
@@ -3,74 +3,54 @@ package com.example.holidayplanner.crawler;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.Logger;
 
 public class Crawler {
 
     public static void main(String[] args) {
-        var url = "https://www.airbnb.co.uk";
+        String url = "https://www.airbnb.co.uk/?checkin=2024-12-18&checkout=2024-12-27&adults=10&price_filter_num_nights=9&price_max=610&price_min=590";
 
-        try {
-            Connection conn = Jsoup.connect(url);
-            Document doc = conn.get();
-
-            if (conn.response().statusCode() == 200) {
-                Elements accommodations = doc.select(" dir dir-ltr");
-
-                for (var accommodation : accommodations) {
-                    ;
-                }
-            }
-        }catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        crawl(1, url, new ArrayList<>());
     }
-    private static void scrape(int level, String url, ArrayList<String> visitedUrls) {
 
+    private  static void crawl(int level, String url, ArrayList<String> visitedUrls) {
         if (level <= 5) {
             Document doc = request(url, visitedUrls);
 
             if (doc != null) {
-                System.out.println("Received web page at " + url);
-                System.out.println(doc.text());
+                for (Element link : doc.select("a[href]")) {
+                    System.out.println(link);
 
-                var links = doc.select("a[href]");
+                    String next_link = link.absUrl("href");
+                    System.out.println(next_link);
 
-                for (var link : links) {
-                    var nextUrl = link.absUrl("href");
-
-                    if (!visitedUrls.contains(nextUrl)) {
-                        scrape(level++, nextUrl, visitedUrls);
+                    if (!visitedUrls.contains(next_link)) {
+                        crawl(level++, next_link, visitedUrls);
                     }
                 }
-            } else {
-                System.out.println("Null document received at " + url);
             }
-
         }
-
     }
 
     private static Document request(String url, ArrayList<String> visited) {
         try {
-            Connection conn = Jsoup.connect(url);
-            Document doc = conn.get();
+           Connection conn = Jsoup.connect(url);
+           Document doc = conn.get();
 
-            if (conn.response().statusCode() == 200) {
-                visited.add(url);
+           if (conn.response().statusCode() == 200){
+               System.out.println(doc.title());
+               System.out.println("Link: " + url);
+               visited.add(url);
 
-                return doc;
-            }
-            return doc;
+               return doc;
+           }
+
+           return null;
         } catch (IOException e) {
-            Logger.getGlobal().warning("Error connecting to " + url);
+            throw new RuntimeException(e);
         }
-        return null;
     }
-
-
 }
