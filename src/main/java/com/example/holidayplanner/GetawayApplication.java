8eb3738e42f5faa88c74e899d2e94f7952dd4b79
Fifolu Akinyemi <fkinyemi@gmail.com>
2024-09-27 10:03:25 +0100

    Remove all scrapper code and revert folder structure.

--- src/main/java/com/example/holidayplanner/GetawayApplication.java
+++ src/main/java/com/example/holidayplanner/GetawayApplication.java
@@ -3,16 +3,15 @@ package com.example.holidayplanner;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication()
 @EnableScheduling
 @OpenAPIDefinition
-public class HolidayPlannerApplication {
+public class GetawayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HolidayPlannerApplication.class, args);
+		SpringApplication.run(GetawayApplication.class, args);
 	}
 
 }
